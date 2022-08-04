0c8699dc9ed30929dc02e918820fc8c613b65800
KrlosPK <centille095@gmail.com>
2022-08-03 22:51:19 -0500

    Proyecto listo hasta la parte de añadir quehaceres

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 const formEl = document.getElementById("form");
 const userInputEl = document.getElementById("userInput");
 const btnSubmitEl = document.getElementById("btnSubmit");
-const todoEl = document.getElementById("todo");
 const todoList = document.getElementById("todos");
 
 // Enviar la información del usuario a la lista
 formEl.addEventListener("submit", function (e) {
     e.preventDefault();
-    // Añadir un nuevo elemento
-    const newToDo = document.createElement("div");
-    newToDo.classList.add("todo");
-    newToDo.textContent = userInputEl.value;
-    todoList.appendChild(newToDo);
+    createToDo();
 });
+
+function createToDo() {
+    // Añadir un nuevo elemento validando si está vacío
+    if (userInputEl.value !== "") {
+        const newToDo = document.createElement("div");
+        newToDo.classList.add("todo");
+        newToDo.textContent = userInputEl.value;
+        todoList.appendChild(newToDo);
+        userInputEl.value = "";
+    } else {
+        return 0;
+    }
+}
 
 // Marcar como completado
-todoEl.addEventListener("click", function () {
-    todoEl.classList.toggle("checked");
-});
+formEl.addEventListener("click", isChecked());
+function isChecked() {
+    var todoEl = document.querySelectorAll("div[class = 'todo']");
+    for (i = 0; i < todoEl.length; i++) {
+        todoEl[i].classList.toggle("checked");
+    }
+}
 
 // Eliminar el quehacer de la lista
-todoEl.addEventListener("contextmenu", function (e) {
-    e.preventDefault();
-    todoEl.remove();
-});
+// todoEl.addEventListener("contextmenu", function (e) {
+//     e.preventDefault();
+//     todoEl.remove();
+// });
