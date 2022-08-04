4601f3637011c43267b7a4243fce3bb720edbe0f
KrlosPK <centille095@gmail.com>
2022-08-03 22:25:33 -0500

    Insrucciones añadidas

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const formEl = document.getElementById("form");
 const userInputEl = document.getElementById("userInput");
 const btnSubmitEl = document.getElementById("btnSubmit");
 const todoEl = document.getElementById("todo");
+const todoList = document.getElementById("todos");
 
 // Enviar la información del usuario a la lista
-formEl.addEventListener("submit", function(e) {
+formEl.addEventListener("submit", function (e) {
     e.preventDefault();
-    todoEl.innerHTML = `${userInputEl.value}`;
+    // Añadir un nuevo elemento
+    const newToDo = document.createElement("div");
+    newToDo.classList.add("todo");
+    newToDo.textContent = userInputEl.value;
+    todoList.appendChild(newToDo);
 });
 
 // Marcar como completado
-todoEl.addEventListener("click", function() {
+todoEl.addEventListener("click", function () {
     todoEl.classList.toggle("checked");
 });
 
 // Eliminar el quehacer de la lista
-todoEl.addEventListener("contextmenu", function(e) {
+todoEl.addEventListener("contextmenu", function (e) {
     e.preventDefault();
     todoEl.remove();
 });
