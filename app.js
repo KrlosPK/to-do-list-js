31cdaf24a2dcb6d63af2b005251f00d8a2d28bea
KrlosPK <centille095@gmail.com>
2022-08-07 14:51:14 -0500

    añadida la función de eliminar quehaceres

--- app.js
+++ app.js
@@ -2,53 +2,51 @@ const formEl = document.getElementById("form");
 const userInputEl = document.getElementById("userInput");
 const btnSubmitEl = document.getElementById("btnSubmit");
 const todoList = document.getElementById("todos");
-// Enviar la información del usuario a la lista
-formEl.addEventListener("submit", function (e) {
-	e.preventDefault();
-	createToDo();
-});
 
 //* lo marca como completado
 function check() {
-	document.querySelectorAll(".todo").forEach((e) => {
-		e.onclick = () => {
-			e.classList.toggle("checked");
-		};
-	});
+    document.querySelectorAll(".todo").forEach((e) => {
+        e.onclick = () => {
+            e.classList.toggle("checked");
+        };
+    });
 }
 check();
 
+// Eliminar el quehacer de la lista
+function erase() {
+    document.querySelectorAll(".todo").forEach((e) => {
+        e.oncontextmenu = (event) => {
+            event.preventDefault();
+            e.remove();
+        };
+    });
+}
+erase();
+
 function createToDo() {
-	// Añadir un nuevo elemento validando si está vacío
-	if (userInputEl.value !== "") {
-		const newToDo = document.createElement("div");
-		const newToDoSpan = document.createElement("span");
-		const newToDoI = document.createElement("i");
-		newToDo.classList.add("todo");
-		newToDoI.classList.add("fa-solid");
-		newToDoI.classList.add("fa-circle-check");
-		newToDoSpan.textContent = userInputEl.value;
-		newToDo.appendChild(newToDoSpan);
-		newToDo.appendChild(newToDoI);
-		todoList.appendChild(newToDo);
-		userInputEl.value = "";
-		check();
-	} else {
-		return;
-	}
+    // Añadir un nuevo elemento validando si está vacío
+    if (userInputEl.value !== "") {
+        const newToDo = document.createElement("div");
+        const newToDoSpan = document.createElement("span");
+        const newToDoI = document.createElement("i");
+        newToDo.classList.add("todo");
+        newToDoI.classList.add("fa-solid");
+        newToDoI.classList.add("fa-circle-check");
+        newToDoSpan.textContent = userInputEl.value;
+        newToDo.appendChild(newToDoSpan);
+        newToDo.appendChild(newToDoI);
+        todoList.appendChild(newToDo);
+        userInputEl.value = "";
+        check();
+        erase();
+    } else {
+        return;
+    }
 }
 
-// Eliminar el quehacer de la lista
-/* function erase() {
-    todoEl.addEventListener("contextmenu", (ev) => {
-        ev.preventDefault();
-        ev.remove();
-    });
-} */
-
-document.querySelectorAll(".todo").forEach((ev) => {
-	ev.oncontextmenu = () => {
-		ev.preventDefault();
-		ev.remove();
-	};
+// Enviar la información del usuario a la lista
+formEl.addEventListener("submit", function (e) {
+    e.preventDefault();
+    createToDo();
 });
